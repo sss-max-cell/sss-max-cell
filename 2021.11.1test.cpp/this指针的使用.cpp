517a6716f1e58186dace1acc2c3c11d0a70e2155
sss-max-cell <3203454391@qq.com>
2021-11-04 20:47:39 +0800

    this指针的使用的完善.cpp

--- 2021.11.1test.cpp/this指针的使用.cpp
+++ 2021.11.1test.cpp/this指针的使用.cpp
@@ -6,8 +6,9 @@ public:
 	//1、解决成员变量与形参重名的情况
 	Person (int age)//这个是构造函数
 	{
-		//this 指向的是被调用成员函数 所属对象 即p1
+		//this 指向的是被调用成员函数 所属对象 即p1。相当于this这个指针指向的就是，新定义的对象
 		this->age = age;
+		//地址用—>，结构体/对象的名字  用.
 	}
 	//2、
 	Person& personaddage(Person &p)
@@ -26,6 +27,7 @@ void test02()
 {
 	Person p1(10);
 	Person p2(10);
+	//p2.personaddage(p1);这个是单独传入时这样写的，p2为对象，调用类中的personaddage函数
 	p2.personaddage(p1).personaddage(p1).personaddage(p1);
 	cout << "p2的年龄 " << p2.age;
 }
